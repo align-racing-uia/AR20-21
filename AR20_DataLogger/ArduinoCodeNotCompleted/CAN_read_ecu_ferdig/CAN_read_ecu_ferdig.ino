631fc65e1f397ffda345bc487fec8778c77f442b
Mohammad Najeeb <mohammad.n.najeeb@gmail.com>
2020-02-11 13:30:44 +0100

    DataLoggerAR20

--- AR20_DataLogger/ArduinoCodeNotCompleted/CAN_read_ecu_ferdig/CAN_read_ecu_ferdig.ino
+++ AR20_DataLogger/ArduinoCodeNotCompleted/CAN_read_ecu_ferdig/CAN_read_ecu_ferdig.ino
@@ -2,15 +2,19 @@
 #include <mcp2515.h>
 #include <SD.h>
 
+int sensor1Pin = 0;    // input for BOSCH 0 280 130 039 (first temp sensor)
+float x1 = 0;       // variable to store the value coming from the sensor
+int sensor2Pin = 1;    // BOSCH 0 280 130 039 (first temp sensor)
+int sensor2 = 0; 
 
 File myFile;
 
-struct can_frame canMsg;
+struct can_frame canMsg1;
 MCP2515 mcp2515(9);
 double count=0;
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   SPI.begin();
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -43,8 +47,11 @@ void setup() {
     myFile.print(",");
     myFile.print("sensor3");
     myFile.print(",");
-    myFile.println("sensor4");
-    
+    myFile.print("sensor4");
+    myFile.print(",");
+    myFile.print("sensor5");
+    myFile.print(",");
+    myFile.println("sensor6");
     myFile.close();
     }else {
     // if the file didn't open, print an error:
@@ -61,40 +68,53 @@ void setup() {
 }
 
 void loop() {
-  
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    x1 = analogRead(sensor1Pin);    // read the value from the sensor
+    float sensor1=((5000)/((x1/1024)*5))-1000;
+    sensor2 = analogRead(sensor2Pin);
+  if (mcp2515.readMessage(&canMsg1) == MCP2515::ERROR_OK) {
         myFile = SD.open("testCAN.txt", FILE_WRITE);
         myFile.print(count);
         myFile.print(" ");
-        myFile.print(canMsg.can_id, HEX); // print ID
+        myFile.print(",");
+        myFile.print(canMsg1.can_id, HEX); // print ID
         myFile.print(" "); 
         myFile.print(",");
-        myFile.print(canMsg.can_dlc, HEX); // print DLC
+        myFile.print(canMsg1.can_dlc, HEX); // print DLC
         myFile.print(" ");
+         myFile.print(",");
 
  // print the data       
-      myFile.print(canMsg.data[0],HEX);
+      myFile.print(canMsg1.data[0],HEX);
       myFile.print(" ");
-      myFile.print(canMsg.data[1],HEX);
+      myFile.print(",");
+      myFile.print(canMsg1.data[1],HEX);
       myFile.print(",");       
-      myFile.print(canMsg.data[2],HEX);
+      myFile.print(canMsg1.data[2],HEX);
       myFile.print(" ");
-      myFile.print(canMsg.data[3],HEX);
+      myFile.print(canMsg1.data[3],HEX);
       myFile.print(",");       
-      myFile.print(canMsg.data[4],HEX);
+      myFile.print(canMsg1.data[4],HEX);
       myFile.print(" ");
-      myFile.print(canMsg.data[5],HEX);
+      myFile.print(canMsg1.data[5],HEX);
       myFile.print(",");      
-      myFile.print(canMsg.data[6],HEX);
+      myFile.print(canMsg1.data[6],HEX);
       myFile.print(" ");
-      myFile.println(canMsg.data[7],HEX);
+      myFile.print(",");
+      myFile.print(canMsg1.data[7],HEX);
       myFile.print(","); 
-   
+      myFile.print(sensor1);
+      myFile.print(" ");
+      myFile.print(",");
+      myFile.println(sensor2);
  
 
     }
+      myFile.print(sensor1);
+      myFile.print(" ");
+      myFile.print(",");
+      myFile.println(sensor2);
       myFile.close();
-      count++;
-      delay(1);
+      count=millis();
+      
     //Serial.println();      
   }
