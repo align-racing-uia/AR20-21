abeea18c84c8e4b9ee4ce919fbd1a5eb27a68270
Simon <Simon>
2020-03-22 14:32:15 +0100

    Settings adjustment

--- AR20_Pedalbox/Pedalbox_code/CANbus Test/CAN_ETB_Write_max/settings.h
+++ AR20_Pedalbox/Pedalbox_code/CANbus Test/CAN_ETB_Write_max/settings.h
@@ -13,7 +13,7 @@ int led = 9;
 
 //Function settings
 const uint8_t min = 0;
-const uint8_t max = 255;
+const uint8_t max = 65; // 255-100% | 128-50% | 51 - 20%
 
 /*
 Frequency
@@ -23,9 +23,9 @@ Frequency
 40Hz = 12
 50Hz = 
 */
-float frequency = 5; //Frequency in hz
+float frequency = 1; //Frequency in hz
 
-float duration_uS = (1000/(frequency)  *1E3)/256;//period_us/max;
+float duration_uS = (1000/(frequency)  *1E3)/max;//period_us/max;
 
 // Input settings
 const uint8_t pedalInput = A3;
