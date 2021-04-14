2029bff26f65fb820776688c24620d802ccdae1c
Adrian Berntsen Hoffsten <adrianberntsen@live.no>
2021-04-14 20:40:49 +0200

    Compiling code

--- AR20_EGS/EGS code V3/src/clutchServo.cpp
+++ AR20_EGS/EGS code V3/src/clutchServo.cpp
@@ -1,109 +1,109 @@
-/*******************************************************************************
-* Copyright 2016 ROBOTIS CO., LTD.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*******************************************************************************/
+// /*******************************************************************************
+// * Copyright 2016 ROBOTIS CO., LTD.
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// *******************************************************************************/
 
-#include <Dynamixel2Arduino.h>
+// #include <Dynamixel2Arduino.h>
 
-// Please modify it to suit your hardware.
-#include <SoftwareSerial.h>
-SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RmX/TX
-#define DXL_SERIAL Serial
-#define DEBUG_SERIAL soft_serial
-const uint8_t DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
+// // Please modify it to suit your hardware.
+// #include <SoftwareSerial.h>
+// SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RmX/TX
+// #define DXL_SERIAL Serial
+// #define DEBUG_SERIAL soft_serial
+// const uint8_t DXL_DIR_PIN = 2; // DYNAMIXEL Shield DIR PIN
 
-const uint8_t DXL_ID = 1;
-const float DXL_PROTOCOL_VERSION = 2.0;
+// const uint8_t DXL_ID = 1;
+// const float DXL_PROTOCOL_VERSION = 2.0;
 
-Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+// Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
 
-//This namespace is required to use Control table item names
-using namespace ControlTableItem;
+// //This namespace is required to use Control table item names
+// using namespace ControlTableItem;
 
-int clutchPin = 3;
+// int clutchPin = 3;
 
-void engageClutch()
-{
-    dxl.torqueOn(DXL_ID);
-    dxl.setGoalCurrent(DXL_ID, 200, UNIT_MILLI_AMPERE);
-}
-void disengageClutch()
-{
-    dxl.setGoalCurrent(DXL_ID, -20, UNIT_MILLI_AMPERE);
-    delay(100);
-    dxl.setGoalCurrent(DXL_ID, 0, UNIT_MILLI_AMPERE);
-    dxl.torqueOff(DXL_ID);
-}
-void clutchInterupt()
-{
-}
-void setup()
-{
-    // put your setup code here, to run once:
-    pinMode(clutchPin, INPUT);
+// void engageClutch()
+// {
+//     dxl.torqueOn(DXL_ID);
+//     dxl.setGoalCurrent(DXL_ID, 200, UNIT_MILLI_AMPERE);
+// }
+// void disengageClutch()
+// {
+//     dxl.setGoalCurrent(DXL_ID, -20, UNIT_MILLI_AMPERE);
+//     delay(100);
+//     dxl.setGoalCurrent(DXL_ID, 0, UNIT_MILLI_AMPERE);
+//     dxl.torqueOff(DXL_ID);
+// }
+// void clutchInterupt()
+// {
+// }
+// void setup()
+// {
+//     // put your setup code here, to run once:
+//     pinMode(clutchPin, INPUT);
 
-    // Use UART port of DYNAMIXEL Shield to debug.
-    DEBUG_SERIAL.begin(9600);
+//     // Use UART port of DYNAMIXEL Shield to debug.
+//     DEBUG_SERIAL.begin(9600);
 
-    // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
-    dxl.begin(57600);
-    // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
-    dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
-    // Get DYNAMIXEL information
-    dxl.ping(DXL_ID);
+//     // Set Port baudrate to 57600bps. This has to match with DYNAMIXEL baudrate.
+//     dxl.begin(57600);
+//     // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
+//     dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+//     // Get DYNAMIXEL information
+//     dxl.ping(DXL_ID);
 
-    // Turn off torque when configuring items in EEPROM area
-    dxl.torqueOff(DXL_ID);
-    dxl.setOperatingMode(DXL_ID, OP_CURRENT);
-    dxl.ledOn(DXL_ID);
-    dxl.torqueOn(DXL_ID);
-    // attachInterrupt(digitalPinToInterrupt(clutchPin), clutchInterupt, CHANGE);
-}
+//     // Turn off torque when configuring items in EEPROM area
+//     dxl.torqueOff(DXL_ID);
+//     dxl.setOperatingMode(DXL_ID, OP_CURRENT);
+//     dxl.ledOn(DXL_ID);
+//     dxl.torqueOn(DXL_ID);
+//     // attachInterrupt(digitalPinToInterrupt(clutchPin), clutchInterupt, CHANGE);
+// }
 
-void loop()
-{
-    // put your main code here, to run repeatedly:
-    if (digitalRead(clutchPin) == HIGH)
-    {
-        engageClutch();
-    }
-    else
-    {
-        disengageClutch();
-    }
-    delay(3000);
-    // // Please refer to e-Manual(http://emanual.robotis.com/docs/en/parts/interface/dynamixel_shield/) for available range of value.
-    // // Set Goal Current using RAW unit
-    // // dxl.torqueOn(DXL_ID);
-    // // dxl.setGoalCurrent(DXL_ID, 200);
-    // // delay(1000);
-    // // // Print present current
-    // // DEBUG_SERIAL.print("Present Current(raw) : ");
-    // // DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID));
-    // // delay(1000);
+// void loop()
+// {
+//     // put your main code here, to run repeatedly:
+//     if (digitalRead(clutchPin) == HIGH)
+//     {
+//         engageClutch();
+//     }
+//     else
+//     {
+//         disengageClutch();
+//     }
+//     delay(3000);
+//     // // Please refer to e-Manual(http://emanual.robotis.com/docs/en/parts/interface/dynamixel_shield/) for available range of value.
+//     // // Set Goal Current using RAW unit
+//     // // dxl.torqueOn(DXL_ID);
+//     // // dxl.setGoalCurrent(DXL_ID, 200);
+//     // // delay(1000);
+//     // // // Print present current
+//     // // DEBUG_SERIAL.print("Present Current(raw) : ");
+//     // // DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID));
+//     // // delay(1000);
 
-    // Set Goal Current using mA unit
-    // dxl.setGoalCurrent(DXL_ID, 200, UNIT_MILLI_AMPERE);
-    // delay(1000);
-    // DEBUG_SERIAL.print("Present Current(mA) : ");
-    // DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID, UNIT_MILLI_AMPERE));
-    // delay(1000);
+//     // Set Goal Current using mA unit
+//     // dxl.setGoalCurrent(DXL_ID, 200, UNIT_MILLI_AMPERE);
+//     // delay(1000);
+//     // DEBUG_SERIAL.print("Present Current(mA) : ");
+//     // DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID, UNIT_MILLI_AMPERE));
+//     // delay(1000);
 
-    // // // Set Goal Current using percentage (-100.0 [%] ~ 100.0[%])
-    // // dxl.setGoalCurrent(DXL_ID, -200, UNIT_PERCENT);
-    // // delay(1000);
-    // // DEBUG_SERIAL.print("Present Current(ratio) : ");
-    // // DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID, UNIT_PERCENT));
-    // // delay(1000);
-}
+//     // // // Set Goal Current using percentage (-100.0 [%] ~ 100.0[%])
+//     // // dxl.setGoalCurrent(DXL_ID, -200, UNIT_PERCENT);
+//     // // delay(1000);
+//     // // DEBUG_SERIAL.print("Present Current(ratio) : ");
+//     // // DEBUG_SERIAL.println(dxl.getPresentCurrent(DXL_ID, UNIT_PERCENT));
+//     // // delay(1000);
+// }
--- AR20_EGS/EGS code V3/src/clutchServo.h
+++ AR20_EGS/EGS code V3/src/clutchServo.h
@@ -1,22 +1,22 @@
-#ifndef ClutchServo_h
-#define ClutchServo_h
+// #ifndef ClutchServo_h
+// #define ClutchServo_h
 
-#include "Arduino.h"
-#include "SoftwareSerial.h"
-#include "Dynamixel2Arduino.h"
+// #include "Arduino.h"
+// #include "SoftwareSerial.h"
+// #include "Dynamixel2Arduino.h"
 
-class GearSensor
-{
-public:
-    GearSensor();
-    int getPosition();
+// class GearSensor
+// {
+// public:
+//     GearSensor();
+//     int getPosition();
 
-private:
-    int convertToGear(int reading);
-    void resetAMT22(uint8_t);
-    void setZeroSPI(uint8_t encoder);
-    void setCSLine(uint8_t encoder, uint8_t csLine);
-    uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine);
-    uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution);
-};
-#endif
+// private:
+//     int convertToGear(int reading);
+//     void resetAMT22(uint8_t);
+//     void setZeroSPI(uint8_t encoder);
+//     void setCSLine(uint8_t encoder, uint8_t csLine);
+//     uint8_t spiWriteRead(uint8_t sendByte, uint8_t encoder, uint8_t releaseLine);
+//     uint16_t getPositionSPI(uint8_t encoder, uint8_t resolution);
+// };
+// #endif
